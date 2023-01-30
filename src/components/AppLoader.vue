e2e2d06181113e2589a28dcdcce657421db5603d
GuglielmoBini <guglielmobini@gmail.com>
2023-01-30 17:45:54 +0100

    added appLoader

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -4,8 +4,20 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div id="loader" class="d-flex flex-column justify-content-center align-items-center">
+        <i class="fa-solid fa-spinner fa-5x fa-spin mb-3"></i>
+        <h2>CARICAMENTO...</h2>
+    </div>
+</template>
 
 <style lang="scss" scoped>
-
+#loader {
+    position: fixed;
+    top: 130px;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba($color: black, $alpha: 0.6);
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,8 @@
 <script>
+// importo
 import { store } from '../data/store';
 import TitlesSection from './titles/TitlesSection.vue';
-import appLoader from './appLoader.vue';
+import appLoader from './AppLoader.vue';
 export default {
     name: 'AppMain',
     data() {
@@ -12,14 +13,16 @@ export default {
 </script>
 
 <template>
+    <!-- MAIN -->
     <main class="container-fluid text-white">
-        <app-loader></app-loader>
         <div v-if="!store.movies.length && !store.series.length"
             class="search-message d-flex justify-content-center align-items-center">
             <h1>CERCA UN FILM O UNA SERIE TV</h1>
         </div>
         <section v-else id="productions">
-            <div v-if="store.isLoading">caricamento...</div>
+            <!-- loader -->
+            <app-loader v-if="store.isLoading"></app-loader>
+            <!-- sezione titoli -->
             <div v-else class="text-center">
                 <titles-section v-if="store.movies.length" title="Movies" collection="movies"></titles-section>
                 <titles-section v-if="store.series.length" title="Series" collection="series"></titles-section>
@@ -31,7 +34,7 @@ export default {
 <style lang="scss" scoped>
 main {
     min-height: 100vh;
-    background-color: #555555;
+    background-color: #555;
     padding: 130px 0 50px 0;
 
     .search-message {
